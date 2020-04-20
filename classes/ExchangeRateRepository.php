4d19a55b5484074c07e1eb5b7e222ac9170f1775
Anatoly <chistowick@yandex.ru>
2020-04-20 22:03:07 +0300

    Рефакторинг класса ExchangeRateRepository

--- classes/ExchangeRateRepository.php
+++ classes/ExchangeRateRepository.php
@@ -1,24 +1,27 @@
 <?php
 
-//Класс позволяет получать, хранить и возвращать по требованию массив 
-//XML-ответов о курсе всех доступных валют на заданные даты. 
-//Предполагается использовать данные из этого массива для реализации 
-//работы объектов, соответствующих отдельной валюте.
+/*
+ * Класс ExchangeRateRepository позволяет получать, хранить и возвращать 
+ * по требованию массив XML-ответов о курсе всех доступных валют 
+ * на заданные даты. Предполагается использовать данные из этого массива 
+ * для реализации работы объектов, соответствующих отдельной валюте.
+ */
 
 class ExchangeRateRepository {
 
-    public $url = 'http://www.cbr.ru/scripts/XML_daily.asp?date_req=';
+    public $url;
+    public $url_template = 'http://www.cbr.ru/scripts/XML_daily.asp?date_req=';
     public $date_req;
-    public $exchange_rates_on_date;
+    public $list_of_exchange_rates_on_date;
 
     //Вернуть элемента массива с данными о курсах валют на выбранную дату
-    public function returnExchangeRatesOnDate($selected_date) {
-        if (!isset($this->exchange_rates_on_date[$selected_date])) {
+    public function returnListOfExchangeRatesOnDate($selected_date) {
+        if (!isset($this->list_of_exchange_rates_on_date[$selected_date])) {
             //если данных на выбранную дату ещё нет - получаем их и сохраняем
             $this->getExchangeRatesOnDate($selected_date);
         }
         //если есть - просто возвращаем результат
-        return $this->exchange_rates_on_date[$selected_date];
+        return $this->list_of_exchange_rates_on_date[$selected_date];
     }
     
     //Получить данные о курсе валют на выбранную дату
@@ -26,7 +29,7 @@ class ExchangeRateRepository {
         //приводим форму даты к подходящему виду для запроса к API банка
         $this->formatDate($selected_date);
 
-        $this->url .= $this->date_req;
+        $this->url = $this->url_template . $this->date_req;
 
         //инициализируем сеанс cURL и настраеваем его
         $this->ch = curl_init();
@@ -34,7 +37,7 @@ class ExchangeRateRepository {
         curl_setopt($this->ch, CURLOPT_URL, $this->url);
 
         //записываем XML-ответ в элемент массива с ключем в виде соответствующей даты
-        $this->exchange_rates_on_date[$selected_date] = curl_exec($this->ch);
+        $this->list_of_exchange_rates_on_date[$selected_date] = curl_exec($this->ch);
 
         curl_close($this->ch);
 
